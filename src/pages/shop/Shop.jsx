bc46d1fb50230679778326ba361e31de806e68e7
Shota Pailodze <55694002+shotapailodze@users.noreply.github.com>
2023-08-29 19:25:32 +0400

    shop context

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {PRODUCTS} from '../../products.js'
+import Product from './Product.jsx'
+import './shop.css'
 
 function Shop() {
   return (
-    <div>Shop</div>
+    <div className='shop'>
+        <div className='shopTitle'>
+            <h1>P's Shop</h1>
+        </div>
+        <div className='products'>
+            {PRODUCTS.map((product) => <Product data={product} />)}
+        </div>
+    </div>
   )
 }
 
